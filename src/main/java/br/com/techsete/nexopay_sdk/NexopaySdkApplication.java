71fb3953e5fcfbb845fc529e70a2d09b3bb1cbb4
Edson Isaac <edson-isaac@hotmail.com>
2025-12-17 13:07:31 -0300

    Initial commit

---
+++ src/main/java/br/com/techsete/nexopay_sdk/NexopaySdkApplication.java
@@ -0,0 +1,13 @@
+package br.com.techsete.nexopay_sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexopaySdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexopaySdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/techsete/nexopay_sdk/NexopaySdkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techsete.nexopay_sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexopaySdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
